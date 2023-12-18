1a1f66e59c3854dc2929ad5b1876d7f54f9a6be2
Rahee <rahee.pm@gmail.com>
2023-12-18 18:37:17 +0600

    Modify

--- TaskManagement/Controllers/TaskController.cs
+++ TaskManagement/Controllers/TaskController.cs
@@ -1,15 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TaskManagement.Interfaces;
+using TaskManagement.Models;
 
 namespace TaskManagement.Controllers
 {
-    public class TaskController : Controller
+    [Route("api/tasks")]
+    [ApiController]
+    public class TaskController : ControllerBase
     {
         private readonly ITaskService _taskService;
 
         public TaskController(ITaskService taskService)
         {
             _taskService = taskService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskModel>>> GetTasks()
+        {
+            var tasks = await _taskService.GetTasks();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{taskId}")]
+        public async Task<ActionResult<TaskModel>> GetTaskById(int taskId)
+        {
+            var task = await _taskService.GetTaskById(taskId);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskModel>> CreateTask([FromBody] TaskModel task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdTask = await _taskService.CreateTask(task);
+            return CreatedAtAction(nameof(GetTaskById), new { taskId = createdTask.Id }, createdTask);
+        }
+
+        [HttpPut("{taskId}")]
+        public async Task<ActionResult<TaskModel>> UpdateTask(int taskId, [FromBody] TaskModel task)
+        {
+            if (taskId != task.Id)
+            {
+                return BadRequest("Mismatched taskId in the request body.");
+            }
+
+            var updatedTask = await _taskService.UpdateTask(task);
+            if (updatedTask == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedTask);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> DeleteTask(int Id)
+        {
+            var result = await _taskService.DeleteTask(Id);
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
 }
---
+++ TaskManagement/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Interfaces;
+using TaskManagement.Models;
+
+namespace TaskManagement.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserModel>> RegisterUser([FromBody] UserModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registeredUser = await _userService.RegisterUser(user);
+            return CreatedAtAction(nameof(GetUserById), new { userId = registeredUser.Id }, registeredUser);
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<ActionResult<UserModel>> AuthenticateUser([FromBody] UserModel userAuth)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var authenticatedUser = await _userService.AuthenticateUser(userAuth.Username, userAuth.Password);
+
+            if (authenticatedUser == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(authenticatedUser);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<UserModel>> GetUserById(int userId)
+        {
+            // Add implementation to retrieve a user by ID if needed.
+            // This action is included for reference purposes.
+            // Adjust it based on your actual requirements.
+            var user = new UserModel(); // Replace with actual logic to get user by ID
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
